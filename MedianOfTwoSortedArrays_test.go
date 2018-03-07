009f78bd0119b7929a9e3e0979e694f9c04534e0
songshawn <553815887@qq.com>
2018-03-07 20:09:56 +0800

    median Of Two sorted arrays

--- MedianOfTwoSortedArrays_test.go
+++ MedianOfTwoSortedArrays_test.go
@@ -6,8 +6,32 @@ func TestMedianOfTSA00(t *testing.T) {
 	nums1 := []int{1, 3}
 	nums2 := []int{2}
 
-	t.Logf("ret %f\n", findMedianSortedArrays(nums1, nums2))
+	if 2.0 != findMedianSortedArrays(nums1, nums2) {
+		t.Error(nums1, nums2)
+	}
 
 	nums1 = []int{1, 2}
 	nums2 = []int{3, 4}
+	if 2.5 != findMedianSortedArrays(nums1, nums2) {
+		t.Error(nums1, nums2)
+	}
+
+	nums1 = []int{}
+	nums2 = []int{}
+	if 0 != findMedianSortedArrays(nums1, nums2) {
+		t.Error(nums1, nums2)
+	}
+
+	nums1 = []int{12, 23}
+	nums2 = []int{1, 2, 3, 4, 5}
+	if 4 != findMedianSortedArrays(nums1, nums2) {
+		t.Error(nums1, nums2)
+	}
+
+	nums1 = []int{1, 23}
+	nums2 = []int{1, 2, 3, 4, 5}
+	if 3 != findMedianSortedArrays(nums1, nums2) {
+		t.Error(nums1, nums2)
+	}
+
 }
